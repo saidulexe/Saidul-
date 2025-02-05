77e07d75708a294bb4ac9c350acef576db899e37
Ariyan Saidul <saidulbhuiyan195@gmail.com>
2025-02-05 13:22:23 +0600

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -35,7 +35,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Please enter the message you want to send to all groups",
-			notification: "NOTIFICATION FROM ADMIN SAIF",
+			notification: "NOTIFICATION FROM ADMIN SAIDUL",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
