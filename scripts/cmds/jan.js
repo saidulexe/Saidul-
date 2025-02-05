7b3a4c381fe13efdb48a894bf9e9a018811b1ac1
Ariyan Saidul <saidulbhuiyan195@gmail.com>
2025-02-05 16:58:08 +0600

    Update jan.js

--- scripts/cmds/jan.js
+++ scripts/cmds/jan.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-	 name: "jan",
+	 name: "saidul",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "jan") {
  return message.reply({
- body: " 「❥︎----ღ᭄_ʜᴇʏ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.♡.\nᥫᩣ ɪᴛ’s 𝚂𝙰𝙸𝙳𝚄𝙻\n\n𝐁𝐎𝐓𝐎𝐖𝐍𝐄𝐑\n𝐒𝐀𝐈𝐅𝐔𝐋」",
+ body: " 「❥︎----ღ᭄_ʜᴇʏ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.♡.\nᥫᩣ ɪᴛ’s 𝚂𝙰𝙸𝙳𝚄𝙻\n\n𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑\n」",
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/c8JdH7A.mp4")
  });
  }
